3b7d5f13a4304e3958ef014a248b80e52f325221
Daniil Novikov <devDaniilNovikov@users.noreply.github.com>
2026-06-12 10:35:43 -0700

    chore: создать каркас Android-проекта
    Добавлен базовый Kotlin Android-проект на ветке dev: Gradle Kotlin DSL, модуль app, Gradle Wrapper 9.4.1, applicationId ru.andrew.application, minSdk 26 и targetSdk 36.
    
    На этом шаге добавлена только пустая Kotlin MainActivity без бизнес-логики, экранов MVP и Java-исходников.

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "ru.andrew.application"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "ru.andrew.application"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
