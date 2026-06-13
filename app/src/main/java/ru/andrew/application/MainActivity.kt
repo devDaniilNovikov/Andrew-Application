a8abef3c775980104ecfe85e8494a1be4c3163dc
Даниил Новиков <daniilnovikov@MacBook-Air-Daniil.local>
2026-06-13 12:28:45 -0700

    Исправление: устранение мерцания при инициализации темы и возврат первичного вызова enableEdgeToEdge

--- app/src/main/java/ru/andrew/application/MainActivity.kt
+++ app/src/main/java/ru/andrew/application/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.andrew.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
@@ -46,6 +47,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         
         setContent {
             val themeViewModel: ThemeViewModel = viewModel(factory = ThemeViewModel.Factory)
--- app/src/main/java/ru/andrew/application/data/repository/ThemeRepositoryImpl.kt
+++ app/src/main/java/ru/andrew/application/data/repository/ThemeRepositoryImpl.kt
@@ -2,12 +2,9 @@ package ru.andrew.application.data.repository
 
 import android.content.Context
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import ru.andrew.application.ui.theme.AppTheme
 import ru.andrew.application.ui.theme.ThemePreferences
 
@@ -16,18 +13,9 @@ class ThemeRepositoryImpl(
     private val externalScope: CoroutineScope
 ) : ThemeRepository {
     private val themePreferences = ThemePreferences(context)
-    private val _themeFlow = MutableStateFlow(AppTheme.SYSTEM)
+    private val _themeFlow = MutableStateFlow(themePreferences.getTheme())
     
     override val themeFlow: StateFlow<AppTheme> = _themeFlow.asStateFlow()
-
-    init {
-        externalScope.launch {
-            val savedTheme = withContext(Dispatchers.IO) {
-                themePreferences.getTheme()
-            }
-            _themeFlow.value = savedTheme
-        }
-    }
 
     override fun setTheme(theme: AppTheme) {
         themePreferences.setTheme(theme)
